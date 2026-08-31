3552cfe75c21a859cf83a24f8452a9fc39892088
Amit Shukla <amitshukla090703@gmail.com>
2026-08-31 14:28:05 +0530

    Data transformation

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
 from networksecurity.entity.config_entity import (
     DataIngestionConfig,
-    TrainingPipelineConfig
+    DataValidationConfig,
+    TrainingPipelineConfig,
+    DataTransformationConfig
 )
 
 import sys
@@ -13,29 +17,56 @@ import sys
 if __name__ == "__main__":
 
     try:
+        # 1. Create Training Pipeline Config
+        training_pipeline_config = TrainingPipelineConfig()
 
-        # Create Training Pipeline Config object
-        trainingpipelineconfig = TrainingPipelineConfig()
-
-        # Create Data Ingestion Config object
-        dataingestionconfig = DataIngestionConfig(
-            trainingpipelineconfig
+        # 2. Create Data Ingestion Config
+        data_ingestion_config = DataIngestionConfig(
+            training_pipeline_config
         )
 
-        # Create Data Ingestion object
+        # 3. Create Data Ingestion Object
         data_ingestion = DataIngestion(
-            dataingestionconfig
+            data_ingestion_config
         )
 
-        logging.info("Initiate the data ingestion")
+        logging.info("Initiating data ingestion")
 
-        # Start data ingestion
-        dataingestionartifact = (
+        # 4. Start Data Ingestion
+        data_ingestion_artifact = (
             data_ingestion.initiate_data_ingestion()
         )
 
-        print(dataingestionartifact)
+        logging.info("Data ingestion completed")
 
+        print(data_ingestion_artifact)
+
+        # 5. Create Data Validation Config
+        data_validation_config = DataValidationConfig(
+            training_pipeline_config
+        )
+
+        # 6. Create Data Validation Object
+        data_validation = DataValidation(
+            data_validation_config=data_validation_config,
+            data_ingestion_artifact=data_ingestion_artifact
+        )
+
+        logging.info("Initiating data validation")
+
+        # 7. Start Data Validation
+        data_validation_artifact = (
+            data_validation.initiate_data_validation()
+        )
+
+        logging.info("Data validation completed")
+
+        print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(training_pipeline_config)
+        logging.info("data transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data transformation ended")
     except Exception as e:
-
         raise NetworkSecurityException(e, sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -3,21 +3,62 @@ import sys
 import pandas as pd
 import numpy as np
 
-"""
-    Data ingestion related constant start with DATA_INGESTION VAR NAME
-"""
-
 TARGET_COLUMN: str = "Result"
+
 PIPELINE_NAME: str = "networksecurity"
+
 ARTIFACT_DIR: str = "ArtifactS"
+
 FILE_NAME: str = "phisingData.csv"
 
 TRAIN_FILE_NAME: str = "train.csv"
+
 TEST_FILE_NAME: str = "test.csv"
 
+
+# Schema file
+SCHEMA_FILE_NAME: str = "schema.yaml"
+
+SCHEMA_FILE_PATH = os.path.join(
+    "data_schema",
+    SCHEMA_FILE_NAME
+)
+
+
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
+
 DATA_INGESTION_DATABASE_NAME: str = "AMITAI"
+
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
+
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_VALID_DIR: str = "validated"
+
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+PREPROCESSING_OBJECT_FILE_NAME: str = "preprocessing.pkl"
+
+"""data transformation related constant start with data_tansformation
+"""
+
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+##knn imputer basically used for nan values
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+}
